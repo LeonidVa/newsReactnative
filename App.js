6f4ce849b75e3a34c66d272157a4795ff62126f8
leonidvaidner <leonid.vaydner@yandex.ru>
2022-11-15 09:23:50 +0400

    async loading fonts

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {gStyles} from "./styles/style";
+import * as Font from 'expo-font'
+import {useCallback, useEffect, useState} from "react";
+import * as SplashScreen from 'expo-splash-screen';
+
+
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [appIsReady, setAppIsReady] = useState(false);
+
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          'mt-bold' : require(`./assets/fonts/Montserrat-Bold.ttf`),
+          'mt-light' : require(`./assets/fonts/Montserrat-Light.ttf`)
+        });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+    return (
+        <View style={gStyles.main}>
+          <Text style={gStyles.title}>vdxvsdxvsdvsdvsdvds</Text>
+
+        </View>
+    );
+
+
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
